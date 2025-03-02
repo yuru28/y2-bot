d749e8747ef4681753df7988ef3cab1e75b17dfa
Takuya Mukohira <takuya.mk96@gmail.com>
2025-03-02 12:57:59 +0900

    Aspect を利用してRemovalPolicyをセットする

--- bin/cdk.ts
+++ bin/cdk.ts
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 
 import 'source-map-support/register';
-import { App } from 'aws-cdk-lib';
+import { App, Aspects, CfnResource, IAspect, RemovalPolicy } from 'aws-cdk-lib';
+import { IConstruct } from 'constructs';
 import { CdkStack } from '../lib/cdk-stack';
 
 import { useCapitalize } from '../lib/utils/useCapitalize';
+
 const { capitalize } = useCapitalize();
 
 const app = new App();
@@ -21,7 +23,7 @@ config['slackWebhookUrl'] = process.env.SLACK_WEBHOOK_URL;
 
 const stackName = `Y2Bot${capitalize(stage)}Stack`;
 
-new CdkStack(app, stackName, {
+const cdkStack = new CdkStack(app, stackName, {
   stage,
   stackName,
   ...config,
@@ -30,3 +32,16 @@ new CdkStack(app, stackName, {
     region: process.env.CDK_DEFAULT_REGION,
   },
 });
+
+class DeletionPolicySetter implements IAspect {
+  // eslint-disable-next-line no-unused-vars
+  constructor(private readonly policy: RemovalPolicy) {}
+
+  visit(node: IConstruct): void {
+    if (node instanceof CfnResource) {
+      node.applyRemovalPolicy(this.policy)
+    }
+  }
+}
+
+Aspects.of(cdkStack).add(new DeletionPolicySetter(RemovalPolicy.DESTROY))
